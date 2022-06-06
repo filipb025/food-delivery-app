43fde131103589daf9473ab5867eff3df4968e6e
filipb025 <filip.bogicevic025@hotmail.com>
2022-06-06 16:16:10 +0200

    Heade Component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import "./index.css";
 import App from "./App";
 import { createRoot } from "react-dom/client";
 
+import { BrowserRouter as Router } from "react-router-dom";
+
 const container = document.getElementById("app");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(
+  <Router>
+    <App tab="home" />
+  </Router>
+);
