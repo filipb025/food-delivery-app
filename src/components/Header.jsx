f223c5c4e1a45977a97e471aab8acce04966fd94
filipb025 <filip.bogicevic025@hotmail.com>
2022-06-07 13:11:22 +0200

    User Auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import Logo from "../img/logo.png";
 import Avatar from "../img/avatar.png";
 import { MdShoppingBasket } from "react-icons/md";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../firebase.config";
+import { async } from "@firebase/util";
 
 const Header = () => {
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const login = async () => {
+    const response = await signInWithPopup(firebaseAuth, provider);
+    console.log(response);
+  };
+
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
       {/* Desktop & Table */}
       <div className="hidden md:flex w-full h-full p-4 items-center justify-between">
-        <div className="flex items-center gap-2">
+        <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} alt="Logo" className="w-8 object-cover" />
           <p className="text-headingColor text-xl font-bold"> City</p>
-        </div>
+        </Link>
 
         <div className="flex items-center gap-8">
           <ul className="flex items-center gap-8">
@@ -35,12 +47,15 @@ const Header = () => {
               <p className="text-xs text-white font-semibold">2</p>
             </div>
           </div>
-
-          <img
-            src={Avatar}
-            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl"
-            alt="User Profile Image"
-          />
+          <div className="relative">
+            <motion.img
+              whileTap={{ scale: 0.6 }}
+              src={Avatar}
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer"
+              alt="User Profile Image"
+              onClick={login}
+            />
+          </div>
         </div>
       </div>
 
