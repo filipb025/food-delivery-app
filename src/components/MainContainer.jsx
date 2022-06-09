873340cbf10c1b5b5f35cc0584bd94975c9b0fac
filipb025 <filip.bogicevic025@hotmail.com>
2022-06-09 21:45:58 +0200

    row section

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,11 +1,50 @@
-import React from "react";
-import Delivery from "../img/delivery.png";
+import React, { useEffect, useState } from "react";
 import HomeContainer from "./HomeContainer";
-
+import RowContainer from "./RowContainer";
+import { motion } from "framer-motion";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { useStateValue } from "../context/StateProvider";
 const MainContainer = () => {
+  const [{ foodItems }, dispatch] = useStateValue();
+  const [scrollValue, setScrollValue] = useState(0);
+
+  useEffect(() => {}, []);
+
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <HomeContainer />
+
+      <section className="w-full my-6">
+        <div className="w-full flex items-center justify-between">
+          <p
+            className="text-2xl font-semibold capitalize text-headingColor uppercase relative
+          before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-br from-orange-400 to-orange-600 transition-all ease-in-out duration-100"
+          >
+            Our fresh & healthy fruits
+          </p>
+          <div className="hidden md:flex gap-3 items-center">
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+              onClick={() => setScrollValue(-200)}
+            >
+              <MdChevronLeft className="text-lg text-white" />
+            </motion.div>
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              onClick={() => setScrollValue(200)}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+            >
+              <MdChevronRight className="text-lg text-white" />
+            </motion.div>
+          </div>
+        </div>
+        <RowContainer
+          scrollValue={scrollValue}
+          flag={true}
+          data={foodItems?.filter((n) => n.category === "fruits")}
+        />
+      </section>
     </div>
   );
 };
