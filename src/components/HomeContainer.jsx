f93b502be6aa5a7e4ded7860d544d9984ea55cfb
filipb025 <filip.bogicevic025@hotmail.com>
2022-06-09 15:16:05 +0200

    Home Component

---
+++ src/components/HomeContainer.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Delivery from "../img/delivery.png";
+import HeroImage from "../img/heroBg.png";
+import { heroData } from "../utils/data";
+
+const HomeContainer = () => {
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+          <p className="text-base text-orange-500 font-semibold">
+            Bike Delivery
+          </p>
+          <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
+            <img
+              className="w-full h-full object-contain"
+              src={Delivery}
+              alt="Delivery image"
+            />
+          </div>
+        </div>
+        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold letter tracking-wide text-headingColor">
+          The Fastest Delivery in{" "}
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
+            Your City
+          </span>
+        </p>
+        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis,
+          molestiae et asperiores repellendus tenetur repudiandae explicabo
+          porro blanditiis dicta nostrum at quisquam animi nemo voluptatum,
+          officia nisi, praesentium error soluta.
+        </p>
+        <button
+          type="button"
+          className="md:w-auto bg-gradient-to-br from-orange-400 to-orange-500 w-full px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-out duration-100"
+        >
+          Order Now
+        </button>
+      </div>
+      <div className="py-2 flex-1 flex items-center relative">
+        <img
+          src={HeroImage}
+          alt="Hero image"
+          className="h-400 w-full lg:w-auto lg:h-650 ml-auto"
+        />
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32 py-32 gap-4 flex-wrap drop-shadow-lg">
+          {heroData &&
+            heroData.map((n) => (
+              <div
+                key={n.id}
+                className="  lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex items-center justify-center flex-col"
+              >
+                <img
+                  src={n.imageSrc}
+                  className="w-20 -mt-10 lg:w-40 lg:-mt-20"
+                  alt="Ice cream image"
+                />
+                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {n.name}
+                </p>
+                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {n.description}
+                </p>
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span>
+                  {n.price}
+                </p>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeContainer;
