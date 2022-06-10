a599aac3f58dc9567fe4f780a64f60dc584655f9
filipb025 <filip.bogicevic025@hotmail.com>
2022-06-10 13:22:14 +0200

    cart component, functionality and ui fixes

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,40 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import HomeContainer from "./HomeContainer";
-import RowContainer from "./RowContainer";
 import { motion } from "framer-motion";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import RowContainer from "./RowContainer";
 import { useStateValue } from "../context/StateProvider";
 import MenuContainer from "./MenuContainer";
+import CartContainer from "./CartContainer";
+
 const MainContainer = () => {
-  const [{ foodItems }, dispatch] = useStateValue();
+  const [{ foodItems, cartShow }, dispatch] = useStateValue();
   const [scrollValue, setScrollValue] = useState(0);
 
-  useEffect(() => {}, []);
+  useEffect(() => {}, [scrollValue, cartShow]);
 
   return (
-    <div className="w-full h-auto flex flex-col items-center justify-center">
+    <div className="w-full h-auto flex flex-col items-center justify-center ">
       <HomeContainer />
 
       <section className="w-full my-6">
         <div className="w-full flex items-center justify-between">
-          <p
-            className="text-2xl font-semibold capitalize text-headingColor uppercase relative
-          before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-br from-orange-400 to-orange-600 transition-all ease-in-out duration-100"
-          >
+          <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
             Our fresh & healthy fruits
           </p>
+
           <div className="hidden md:flex gap-3 items-center">
             <motion.div
               whileTap={{ scale: 0.75 }}
-              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer  hover:shadow-lg flex items-center justify-center"
               onClick={() => setScrollValue(-200)}
             >
               <MdChevronLeft className="text-lg text-white" />
             </motion.div>
             <motion.div
               whileTap={{ scale: 0.75 }}
-              onClick={() => setScrollValue(200)}
               className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+              onClick={() => setScrollValue(200)}
             >
               <MdChevronRight className="text-lg text-white" />
             </motion.div>
@@ -46,7 +46,10 @@ const MainContainer = () => {
           data={foodItems?.filter((n) => n.category === "fruits")}
         />
       </section>
+
       <MenuContainer />
+
+      {cartShow && <CartContainer />}
     </div>
   );
 };
--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,28 +1,47 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MdShoppingBasket } from "react-icons/md";
 import { motion } from "framer-motion";
 import NotFound from "../img/NotFound.svg";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
 
 const RowContainer = ({ flag, data, scrollValue }) => {
   const rowContainer = useRef();
+
+  const [items, setItems] = useState([]);
+
+  const [{ cartItems }, dispatch] = useStateValue();
+
+  const addtocart = () => {
+    dispatch({
+      type: actionType.SET_CARTITEMS,
+      cartItems: items,
+    });
+    localStorage.setItem("cartItems", JSON.stringify(items));
+  };
+
   useEffect(() => {
     rowContainer.current.scrollLeft += scrollValue;
   }, [scrollValue]);
 
+  useEffect(() => {
+    addtocart();
+  }, [items]);
+
   return (
     <div
       ref={rowContainer}
-      className={`w-full flex items-center my-12 scroll-smooth ${
+      className={`w-full flex items-center gap-3  my-12 scroll-smooth  ${
         flag
           ? "overflow-x-scroll scrollbar-none"
           : "overflow-x-hidden flex-wrap justify-center"
       }`}
     >
-      {data.length > 0 ? (
+      {data && data.length > 0 ? (
         data.map((item) => (
           <div
-            key={item.id}
-            className="w-275 h-[225px] min-w-[275px] md:min-w-[340px] gap-3 md:w-300 bg-cardOverlay p-2 rounded-lg my-12 backdrop-blur-lg bg-cardOverlay hover:drop-shadow-lg flex flex-col items-center justify-between"
+            key={item?.id}
+            className="w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4  my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative"
           >
             <div className="w-full flex items-center justify-between">
               <motion.div
@@ -30,19 +49,21 @@ const RowContainer = ({ flag, data, scrollValue }) => {
                 whileHover={{ scale: 1.2 }}
               >
                 <img
-                  src={item?.imageUrl}
+                  src={item?.imageURL}
                   alt=""
                   className="w-full h-full object-contain"
                 />
               </motion.div>
               <motion.div
                 whileTap={{ scale: 0.75 }}
-                className="w-8 h-8 rounded-full bg-red-700 flex items-center justify-center cursor-pointer hover:shadow-md"
+                className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md -mt-8"
+                onClick={() => setItems([...cartItems, item])}
               >
                 <MdShoppingBasket className="text-white" />
               </motion.div>
             </div>
-            <div className="w-full flex flex-col gap-4 items-end justify-end">
+
+            <div className="w-full flex flex-col items-end justify-end -mt-8">
               <p className="text-textColor font-semibold text-base md:text-lg">
                 {item?.title}
               </p>
@@ -59,9 +80,9 @@ const RowContainer = ({ flag, data, scrollValue }) => {
         ))
       ) : (
         <div className="w-full flex flex-col items-center justify-center">
-          <img className="h-340" src={NotFound} alt="NOT FOUND" />
-          <p className="text-xl text-headingColor font-semibold">
-            Items not available
+          <img src={NotFound} className="h-340" />
+          <p className="text-xl text-headingColor font-semibold my-2">
+            Items Not Available
           </p>
         </div>
       )}
